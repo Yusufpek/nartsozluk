094af3f90a7db28ae0a2fa75cd39a66451c44672
Yusufpek <yusuf55ipek55@gmail.com>
2024-07-29 17:25:45 +0300

    fav and follow urls added

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # /
     path('', views.HomeView.as_view(), name="index"),
     path('today', views.TodayView.as_view(), name="today"),
+    path('follow', views.FollowView.as_view(), name="follow"),
+    path('fav', views.FavView.as_view(), name="fav"),
     path('<int:title_id>/title', views.TitleView.as_view(), name="title"),
     path('<int:title_id>/voted', views.VotedView.as_view(), name="most-voted"),
     path('<int:author_id>/profile',
