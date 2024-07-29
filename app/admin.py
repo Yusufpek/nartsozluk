7d8a283a3f3a57fa0a010c9ce8504df3d9066754
Yusufpek <yusuf55ipek55@gmail.com>
2024-07-29 16:36:53 +0300

    admin models added

---
+++ app/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import Title, Entry, Author, Vote
+
+# Register your models here.
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ["text", "created_at"]
+    list_filter = ["created_at"]
+
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ["text", "title", "author"]
+    fieldsets = [
+        (
+            "Informations", {"fields": [
+                "title",
+                "author"]},
+        ),
+        ("Text", {"fields": ["text"]}),
+    ]
+    list_filter = ["created_at", "updated_at"]
+
+
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Author)
+admin.site.register(Vote)
