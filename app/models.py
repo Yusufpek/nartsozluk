e9bb6513ab69cba88da5a877bb53e7f5dae59693
Yusufpek <yusuf55ipek55@gmail.com>
2024-07-31 12:23:07 +0300

    count field updated

--- app/models.py
+++ app/models.py
@@ -7,10 +7,8 @@ from .constants import COUNT_CHOICES
 
 # Create your models here.
 class Author(AbstractUser):
-    random_entry_count = models.CharField(
-        max_length=2, default='10', choices=COUNT_CHOICES)
-    title_entry_count = models.CharField(
-        max_length=2, default='10', choices=COUNT_CHOICES)
+    random_entry_count = models.IntegerField(default=10, choices=COUNT_CHOICES)
+    title_entry_count = models.IntegerField(default=10, choices=COUNT_CHOICES)
 
     def __str__(self):
         return self.username
