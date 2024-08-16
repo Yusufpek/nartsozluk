b9539e2f9d92b05ee092f902b9898123e0452bc4
Yusufpek <yusuf55ipek55@gmail.com>
2024-08-16 15:31:02 +0300

    profile attr added to model

--- authentication/models.py
+++ authentication/models.py
@@ -14,6 +14,11 @@ class Author(AbstractUser):
         max_length=2, default='10', choices=COUNT_CHOICES)
     profile_image = models.ImageField(
         upload_to='profile_pictures', default='default.jpg')
+    entry_count = models.IntegerField(default=0)
+    title_count = models.IntegerField(default=0)
+    follower_count = models.IntegerField(default=0)
+    vote_count = models.IntegerField(default=0)
+    up_vote_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.username
