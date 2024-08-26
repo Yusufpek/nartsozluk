3f3f976fa283c462d892f2e9d124e17b2ed8f83a
Yusufpek <yusuf55ipek55@gmail.com>
2024-08-26 14:29:54 +0300

    refcator models

--- app/models.py
+++ app/models.py
@@ -24,7 +24,10 @@ class TimescaleModel(models.Model):
 class Topic(models.Model):
     text = models.CharField(max_length=50)
     created_by = models.ForeignKey(
-        Author, null=True, blank=True, on_delete=models.SET_NULL)
+        Author,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -32,11 +35,21 @@ class Topic(models.Model):
 
 
 class Title(models.Model):
-    text = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
-    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
-    owner = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL)
+    text = models.CharField(
+        max_length=100,
+        validators=[MinLengthValidator(3)])
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True)
+    owner = models.ForeignKey(
+        Author,
+        null=True,
+        on_delete=models.SET_NULL)
     topic = models.ForeignKey(
-        Topic, null=True, blank=True, on_delete=models.SET_NULL)
+        Topic,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.text
@@ -44,15 +57,24 @@ class Title(models.Model):
 
 class Entry(TimescaleModel):
     uid = models.UUIDField(default=uuid.uuid4, editable=False)
-    content = CKEditor5Field(max_length=2000, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(Author,  null=True, on_delete=models.SET_NULL)
-    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    content = CKEditor5Field(
+        max_length=2000,
+        null=True,
+        blank=True)
+    author = models.ForeignKey(
+        Author,
+        null=True,
+        on_delete=models.SET_NULL)
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE)
 
     class Meta:
         indexes = [
             models.Index(fields=['created_at', 'author'])]
 
+    @property
     def is_edited(self):
         compare_created_at = self.created_at.strftime("%B %d %Y - %I:%M %p")
         compare_updated_at = self.updated_at.strftime("%B %d %Y - %I:%M %p")
@@ -64,23 +86,31 @@ class Entry(TimescaleModel):
 
 class Vote(models.Model):
     entry_id = models.UUIDField()
-    voter = models.ForeignKey(Author, on_delete=models.CASCADE)
     is_up = models.BooleanField(default=True)
+    voter = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE)
 
 
 class AuthorsFavorites(models.Model):
     entry_id = models.UUIDField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('entry_id', 'author')  # for no duplicate favorites
 
 
 class FollowAuthor(models.Model):
-    user = models.ForeignKey(Author, on_delete=models.CASCADE,
-                             related_name="following")
-    follow = models.ForeignKey(Author, on_delete=models.CASCADE,
-                               related_name="followers")
+    user = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="following")
+    follow = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="followers")
     follow_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -88,16 +118,22 @@ class FollowAuthor(models.Model):
 
 
 class FollowTitle(models.Model):
-    title = models.ForeignKey(Title, on_delete=models.CASCADE)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE)
     last_seen = models.DateTimeField(auto_now=True)
 
 
 class Report(models.Model):
     entry_uid = models.UUIDField()
     date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
+    user = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE)
 
     def __str__(self):
         return self.description + '- entry: ' + str(self.entry_uid)
