9ee8f122a890c6283ecc14dfb425f8894a6d687a
Yusufpek <yusuf55ipek55@gmail.com>
2024-08-29 14:08:21 +0300

    add: ai funcs to celery

--- dictionary/tasks.py
+++ dictionary/tasks.py
@@ -2,14 +2,14 @@ from celery import shared_task
 from django.core import management
 
 from .ai_utils import AI, create_entry
-from .models import Title, Topic, Author
+from .models import Title, Topic, Author, Entry
 from log.models import Log
 
 
 @shared_task
 def create_random_entries_task(count, entry_count):
     log = Log(
-        task_name="Create Random Entries",
+        task_name="Create Random Entries - {}".format(count*entry_count),
         category=Log.Category.MANAGE
     )
     log.save()
@@ -27,7 +27,7 @@ def create_random_entries_task(count, entry_count):
 @shared_task
 def create_ai_title_task(title_count, entry_count):
     log = Log(
-            task_name="Create Title with AI",
+            task_name="Create Title AI - {}".format(title_count*entry_count),
             category=Log.Category.ENTRY,
         )
     log.save()
@@ -102,6 +102,39 @@ def create_new_entries_to_title_task(form_title, entry_count):
             return output
         log.complete_task_error("Title not found!")
         return "Title not found"
+    except Exception as e:
+        log.complete_task_error(str(e))
+        return str(e)
+
+
+@shared_task
+def create_entries_like_an_entry_task(entry_id, title_id, count):
+    log = Log(
+            task_name="Create Title with AI",
+            category=Log.Category.ENTRY,
+        )
+    log.save()
+
+    entry = Entry.objects.filter(uid=entry_id).first()
+    title = Title.objects.filter(pk=title_id).first()
+    if not (title and entry):
+        out = "not found title and entry"
+        log.complete_task_error(out)
+        return out
+
+    user = Author.objects.filter(username='bot').first()
+    if not user:
+        output = "no user found as a bot"
+        log.complete_task_error(output)
+        return output
+
+    try:
+        ai = AI()
+        entry_res = ai.get_entries_like_an_entry(title, entry, count)
+        for res in entry_res:
+            create_entry(res, user, title)
+        log.complete_task("Like an entries added to title")
+        return "Entries added"
     except Exception as e:
         log.complete_task_error(str(e))
         return str(e)
